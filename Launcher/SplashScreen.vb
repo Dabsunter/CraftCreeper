da420bb62fd359f2b49363ff9bfe36f896733f8d
Dabsunter <dav.nico@orange.fr>
2016-05-27 20:19:57 +0200

    Clean up

--- Launcher/SplashScreen.vb
+++
@@ -1,6 +0,0 @@
-﻿Public NotInheritable Class SplashScreen
-    Sub PlaySound() Handles MyBase.Load
-        Dim Sound As New System.Media.SoundPlayer(My.Resources.SplashSound)
-        Sound.Play()
-    End Sub
-End Class
